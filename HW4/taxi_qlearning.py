24ecfce93b19864ff35bfbb618bbac3745bbe750
wchas <chast068@umn.edu>
2025-04-10 11:45:37 -0500

    started hw4

--- HW4/taxi_qlearning.py
+++ HW4/taxi_qlearning.py
@@ -3,11 +3,9 @@ import gymnasium as gym
 import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm
+import pickle
 
-'''
-I took this definition from https://gymnasium.farama.org/introduction/train_agent/
-'''
-class TaxiQLearnAgent:
+class TaxiQLearningAgent:
     def __init__(
         self,
         env: gym.Env,
@@ -73,3 +71,81 @@ class TaxiQLearnAgent:
 
     def decay_epsilon(self):
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+# hyperparameters
+learning_rate = 0.005
+n_episodes = 1000000
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+final_epsilon = 0.1
+# , sab=False
+env = gym.make("Taxi-v3")
+env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
+
+agent = TaxiQLearningAgent(
+    env=env,
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
+)
+
+'''
+Train agent
+'''
+for episode in tqdm(range(n_episodes)):
+    obs, info = env.reset()
+    done = False
+
+    # play one episode
+    while not done:
+        action = agent.get_action(obs)
+        next_obs, reward, terminated, truncated, info = env.step(action)
+
+        # update the agent
+        agent.update(obs, action, reward, terminated, next_obs)
+
+        # update if the environment is done and the current obs
+        done = terminated or truncated
+        obs = next_obs
+    agent.decay_epsilon()
+
+'''
+Ploting pretty graphs
+'''
+def get_moving_avgs(arr, window, convolution_mode):
+    return np.convolve(
+        np.array(arr).flatten(),
+        np.ones(window),
+        mode=convolution_mode
+    ) / window
+
+# Smooth over a 500 episode window
+rolling_length = 500
+fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
+
+axs[0].set_title("Episode rewards")
+reward_moving_average = get_moving_avgs(
+    env.return_queue,
+    rolling_length,
+    "valid"
+)
+axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
+
+axs[1].set_title("Episode lengths")
+length_moving_average = get_moving_avgs(
+    env.length_queue,
+    rolling_length,
+    "valid"
+)
+axs[1].plot(range(len(length_moving_average)), length_moving_average)
+
+axs[2].set_title("Training Error")
+training_error_moving_average = get_moving_avgs(
+    agent.training_error,
+    rolling_length,
+    "same"
+)
+axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
+plt.tight_layout()
+plt.show()
