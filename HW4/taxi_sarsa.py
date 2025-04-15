a0499352b73e25e7f5aed34936d0860f948a84e5
tubbleWeek <wchastek@gmail.com>
2025-04-14 21:08:46 -0500

    added random

--- HW4/taxi_sarsa.py
+++ HW4/taxi_sarsa.py
@@ -9,7 +9,7 @@ import sys
 '''
 Based on agent from https://gymnasium.farama.org/introduction/train_agent/
 '''
-class TaxiQLearningAgent:
+class TaxiSARSAAgent:
     def __init__(
         self,
         env: gym.Env,
@@ -65,7 +65,7 @@ class TaxiQLearningAgent:
         next_action: int
     ):
         """Updates the Q-value of an action."""
-        future_q_value = (not terminated) * np.max(self.q_values[next_obs][next_action])
+        future_q_value = (not terminated) * self.q_values[next_obs][next_action]
         temporal_difference = (
             reward + self.discount_factor * future_q_value - self.q_values[obs][action]
         )
@@ -87,7 +87,7 @@ def main(exploration_param, learning_rt, discount_f):
     # n_episodes = 1000000
     # learning_rate = 0.01
     learning_rate = learning_rt
-    n_episodes = 100_000
+    n_episodes = 100_00
     # start_epsilon = 1.
     start_epsilon = exploration_param
     epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
@@ -96,7 +96,7 @@ def main(exploration_param, learning_rt, discount_f):
     env = gym.make("Taxi-v3")
     env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
 
-    agent = TaxiQLearningAgent(
+    agent = TaxiSARSAAgent(
         env=env,
         learning_rate=learning_rate,
         initial_epsilon=start_epsilon,
@@ -129,19 +129,31 @@ def main(exploration_param, learning_rt, discount_f):
     '''
 
     # dict with dict as value
+    q_vals = agent.get_qvals()
+    policy = {state: int(np.argmax(q_vals[state])) for state in q_vals}
+    q_vals_dict = {state: {action: float(q_vals[state][action]) for action in range(6)} for state in q_vals}
     with open("sarsa_q_vals.pickle", "wb") as f:
-        q_vals = agent.get_qvals()
-        q_vals = pickle.dumps(q_vals)
-        pickle.dump(q_vals, f)
-
-
+        pickle.dump(q_vals_dict, f)
     # just dict
     with open("sarsa_policy.pickle", "wb") as f:
-        q_vals = agent.get_qvals()
-        for state in q_vals:
-            pickle.dump(q_vals[state], f)
+        pickle.dump(policy, f)
 
 
+    '''Random Agent'''
+    rand_env = gym.make("Taxi-v3")
+    rand_env = gym.wrappers.RecordEpisodeStatistics(rand_env, buffer_length=n_episodes)
+    for episode in tqdm(range(n_episodes)):
+        obs, info = rand_env.reset()
+        done = False
+        # print(rand_env.action_space.sample())
+        # play one episode
+        while not done:
+            action = env.action_space.sample()
+            next_obs, reward, terminated, truncated, info = rand_env.step(action)
+
+            # update if the environment is done and the current obs
+            done = terminated or truncated
+            obs = next_obs
     '''
     Ploting pretty graphs
     '''
@@ -154,33 +166,42 @@ def main(exploration_param, learning_rt, discount_f):
 
     # Smooth over a 500 episode window
     rolling_length = 500
-    fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
+    fig, axs = plt.subplots(ncols=2, figsize=(12, 5))
 
-    axs[0].set_title("Episode rewards")
+    axs[0].set_title("SARSA Agent rewards")
     reward_moving_average = get_moving_avgs(
         env.return_queue,
         rolling_length,
         "valid"
     )
     axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
-
-    axs[1].set_title("Episode lengths")
-    length_moving_average = get_moving_avgs(
-        env.length_queue,
+    axs[1].set_title("Random Agent Rewards")
+    random_enviroment_rewards = get_moving_avgs(
+        rand_env.return_queue,
         rolling_length,
         "valid"
     )
-    axs[1].plot(range(len(length_moving_average)), length_moving_average)
-
-    axs[2].set_title("Training Error")
-    training_error_moving_average = get_moving_avgs(
-        agent.training_error,
-        rolling_length,
-        "same"
-    )
-    axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
+    axs[1].plot(range(len(random_enviroment_rewards)), random_enviroment_rewards)
     plt.tight_layout()
-    plt.show()
+    # plt.show()
+    plt.savefig("qlearning total reward.png", transparent=False)
+    # axs[1].set_title("Episode lengths")
+    # length_moving_average = get_moving_avgs(
+    #     env.length_queue,
+    #     rolling_length,
+    #     "valid"
+    # )
+    # axs[1].plot(range(len(length_moving_average)), length_moving_average)
+
+    # axs[2].set_title("Training Error")
+    # training_error_moving_average = get_moving_avgs(
+    #     agent.training_error,
+    #     rolling_length,
+    #     "same"
+    # )
+    # axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
+    # plt.tight_layout()
+    # plt.show()
 
 if __name__ == "__main__":
     exploration_parameter = (float)(sys.argv[1])
